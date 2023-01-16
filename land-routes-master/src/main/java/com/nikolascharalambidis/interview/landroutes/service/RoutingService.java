2c296ea556f8432e8971e8d028a765ad8a081735
acsirbu <51442843+acsirbu@users.noreply.github.com>
2023-01-16 12:11:44 +0200

    Delete land-routes-master/src directory

--- land-routes-master/src/main/java/com/nikolascharalambidis/interview/landroutes/service/RoutingService.java
+++
@@ -1,8 +0,0 @@
-package com.nikolascharalambidis.interview.landroutes.service;
-
-import com.nikolascharalambidis.interview.landroutes.api.routing.model.Route;
-
-public interface RoutingService {
-
-	Route route(String origin, String destination);
-}
